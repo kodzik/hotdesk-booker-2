2222d6eebe0e160e9178830c29e7284c34238450
kodzik <pawlopoczta@wp.pl>
2023-09-13 18:16:50 +0200

    Firebase init

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire/compat';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LayoutComponent } from './layout/layout.component';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [SignInComponent, SignUpComponent, LayoutComponent],
@@ -20,6 +22,7 @@ import { InputTextModule } from 'primeng/inputtext';
     ButtonModule,
     CardModule,
     InputTextModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
 })
 export class AuthModule {}
