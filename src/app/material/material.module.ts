7bd945c8079645b175218cfc088560e70a6d63c0
kodzik <pawlopoczta@wp.pl>
2023-09-26 20:02:37 +0200

    SignIn form layout refactor

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,6 +3,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   imports: [
@@ -10,12 +11,14 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatInputModule,
     MatToolbarModule,
     MatProgressSpinnerModule,
+    MatCardModule,
   ],
   exports: [
     MatButtonModule,
     MatInputModule,
     MatToolbarModule,
     MatProgressSpinnerModule,
+    MatCardModule,
   ],
 })
 export class MaterialModule {}
