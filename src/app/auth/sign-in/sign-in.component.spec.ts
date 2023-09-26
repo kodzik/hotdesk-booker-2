7f9e6477ae47af5b84eda8184cb9855308079265
kodzik <pawlopoczta@wp.pl>
2023-09-26 13:53:34 +0200

    Files refactor

--- src/app/auth/sign-in/sign-in.component.spec.ts
+++ src/app/auth/sign-in/sign-in.component.spec.ts
@@ -3,17 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SignInComponent } from './sign-in.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ReactiveFormsModule, ButtonModule],
       declarations: [SignInComponent],
+      providers: [provideMockStore()],
     }).compileComponents();
 
+    store = TestBed.inject(MockStore);
     fixture = TestBed.createComponent(SignInComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
