14d1b3fe0c13c2cd1896e3fae554277bf09fbe52
kodzik <pawlopoczta@wp.pl>
2024-01-22 14:06:55 +0100

    Layout file fix

--- src/app/auth/layout/layout.component.ts
+++ src/app/auth/layout/layout.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
-  templateUrl: './layout.component.html',
-  styleUrls: ['./layout.component.scss']
+  template: `<div class="account-container">
+    <div class="account-subcontainer">
+      <router-outlet></router-outlet>
+    </div>
+  </div> `,
+  styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
